25c4342d0fa4c0a2829b58a14e3cc7e5f7f03236
stanx974 <stanx974@gmail.com>
2022-09-15 11:25:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,21 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Couvreur 95 Val d'Oise et IDF, Spécialiste Couverture et Toiture - Werner Pro - WERNER PRO SERVICES !" />
+    
+    <center><a href="tel:0658679984"><img src="https://werner-couvreur-95.fr/wp-content/uploads/2022/08/06-58-67-99-84.png" ></a>
+    
+    
+        <Header title="Couvreur 95 Val d'Oise et IDF, - Werner Pro" />
+     <h1>Couvreur 95 Val d'oise : werner pro</h1>
+    
+  <h2> UNE TOITURE A RENOVER : Faites appel à Couvreur 95 - WERNER PRO </h2>
+  <h2>Comment rénover sa toiture</h2>
+  <h2>Quel est prix moyen pour la réfection d'une toiture ?</h2>
+  <h2>Quelles aides pour refaire sa toiture ?</h2>
+  <h2>Quelles aides pour refaire une toiture en 2022 ?</h2>
+  <h2>Comment refaire sa toiture gratuitement ?</h2>
+
+    
     <body>
     
 Ils doivent savoir utiliser différents outils et matériaux. Leur travail consiste également à réparer les toits. Ils doivent donc savoir détecter les fuites et les réparer. Les couvreurs artisans doivent être polyvalents. En effet, ils doivent être capables de travailler avec différents types de toitures. Ils doivent également être capables de s'adapter aux conditions météorologiques.
