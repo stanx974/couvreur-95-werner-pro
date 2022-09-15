a84c0de7c25890e575e61c737bc7a13d6ac4066e
stanx974 <stanx974@gmail.com>
2022-09-15 11:18:30 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Couvreur 95 Val d'Oise et IDF, Spécialiste Couverture et Toiture - Werner Pro - WERNER PRO SERVICES !</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Couvreur 95 Val d'Oise et IDF, Spécialiste Couverture et Toiture - Werner Pro - WERNER PRO SERVICES !" />
+    <body>
+    
+Ils doivent savoir utiliser différents outils et matériaux. Leur travail consiste également à réparer les toits. Ils doivent donc savoir détecter les fuites et les réparer. Les couvreurs artisans doivent être polyvalents. En effet, ils doivent être capables de travailler avec différents types de toitures. Ils doivent également être capables de s'adapter aux conditions météorologiques.
+Le métier de couvreur artisan est un métier très physique. Les couvreurs artisans doivent donc avoir une bonne condition physique. Ils doivent également être habiles de leurs mains. Leur travail consiste également à réparer les toits. Ils doivent donc savoir détecter les fuites et les réparer.
+
+    Pour contacter les équipes <a href="https://werner-couvreur-95.fr/">couvreur 95</a> de werner pro : <a href="https://werner-couvreur-95.fr/">le site officiel</a>
+    </body>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+       
         </p>
       </main>
 
